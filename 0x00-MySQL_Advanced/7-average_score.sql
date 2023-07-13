2289de1549188742f515268f80490400380801c8
waley-code <oyewaleadeosun@yahoo.com>
2023-07-14 00:45:26 +0100

    updated comment in file 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a 
 -- stored procedure ComputeAverageScoreForUser
---- that computes and store the average score for a student.
+-- that computes and store the average score for a student.
 
 DELIMITER $$
 
