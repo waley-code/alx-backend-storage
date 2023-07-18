b415a6f228366160d9492104dffa95850142ddaf
wale_code <walephlp@gmail.com>
2023-07-18 04:43:41 -0700

    complete task 12 and update task 2

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,6 +2,7 @@
 """ MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
     """ Provides some stats about Nginx logs stored in MongoDB """
     client = MongoClient('mongodb://127.0.0.1:27017')
